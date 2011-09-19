6c107399e23aae147e0b481ecc44995f8b147dd2
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2011-09-18 21:46:28 -0400

    xcode generated code for bottom nav with two views

---
+++ randcom/Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  randcom
+//
+//  Created by Sandy D Vanderbleek199 on 9/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ randcom/Classes/randcomAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  randcomAppDelegate.h
+//  randcom
+//
+//  Created by Sandy D Vanderbleek199 on 9/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface randcomAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
